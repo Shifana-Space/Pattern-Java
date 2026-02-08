f9f4c82ab65f8ef0e0360ce5f9533bc2d9be6937
SHIFANA N <shifanasportal@gmail.com>
2026-02-08 14:11:01 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class a {
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       for (int i = 1; i <= n; i++) {
+         for(int j = 1 ; j<=2*n-1 ; j++ ){
+            if(j>n-i && j<n+i){
+                System.out.print("*");
+         }
+         else{
+            System.out.print(" ");
+         }
+         }
+         System.out.println();
+    }
+}
+}
